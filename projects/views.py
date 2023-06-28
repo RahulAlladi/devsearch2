b239b308f575e5c3c9656552752fb7d9b35afa00
RahulAlladi <rahulalladifeb10@gmail.com>
2023-06-28 13:58:20 +0530

    modified code to display data from the database

--- projects/views.py
+++ projects/views.py
@@ -1,33 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Project
 
-projectsList = [
-    {
-        'id':'1',
-        'title':'ecommerce websie',
-        'description':'something like amazon'
-    },
-     {
-        'id':'2',
-        'title':'ecommerce websie2',
-        'description':'something like amazon2'
-    },
-     {
-        'id':'3',
-        'title':'ecommerce websie3',
-        'description':'something like amazon3'
-    }
-]
+
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page' : page,'number':number,'projects':projectsList}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    projectObj = None
-    for  i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request,'projects/single-project.html',{'project':projectObj})
