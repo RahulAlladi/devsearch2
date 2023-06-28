c1118dfdc165c931ae4a53f0bf38dc338f5f2233
RahulAlladi <rahulalladifeb10@gmail.com>
2023-06-28 11:33:02 +0530

    added sample projects,used jinja examples like {{if}} {{for}}

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 
+projectsList = [
+    {
+        'id':'1',
+        'title':'ecommerce websie',
+        'description':'something like amazon'
+    },
+     {
+        'id':'2',
+        'title':'ecommerce websie2',
+        'description':'something like amazon2'
+    },
+     {
+        'id':'3',
+        'title':'ecommerce websie3',
+        'description':'something like amazon3'
+    }
+]
+
 def projects(request):
-    return render(request,'projects/projects.html')
+    page = 'projects'
+    number = 10
+    context = {'page' : page,'number':number,'projects':projectsList}
+    return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    projectObj = None
+    for  i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request,'projects/single-project.html',{'project':projectObj})
